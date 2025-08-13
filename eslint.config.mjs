ddd440ff1d719393a39fdc8c808b9ef9b1212806
ogaguinaga <omar.aguinaga94@gmail.com>
2025-08-12 19:15:32 -0600

    feat: add browser component

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -65,6 +65,7 @@ export default antfu(
   },
   {
     rules: {
+      "style/multiline-ternary": ["off"],
       "antfu/no-top-level-await": "off", // Allow top-level await
       "style/brace-style": ["error", "1tbs"], // Use the default brace style
       "ts/consistent-type-definitions": ["error", "type"], // Use `type` instead of `interface`
